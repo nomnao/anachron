fe6952e7e72ea9d2772bf7ec79d6e34015e5b030
nomnao <aomiaki2022@gmail.com>
2026-09-25 13:06:58 +1000

    Redesign monitor as beige CRT with destop case

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('ANACHRON SYSTEM: main.js loaded');
+const monitor = document.querySelector('#monitor');
+const crtScreen = document.querySelector('#crt-screen');
+const powerButton = document.querySelector('#power-button');
+
+let systemState = 'off';
+
+powerButton.addEventListener('click', () => {
+  if (systemState !== 'off') return;
+
+  systemState = 'booting';
+  monitor.classList.add('is-on');
+  crtScreen.textContent = 'ANACHRON SYSTEM';
+
+  console.log('System state:', systemState);
+});
